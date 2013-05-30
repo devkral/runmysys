32f6618dd0d71b8c06bc744c153a974cee74c9b0
alex <devkral@web.de>
2013-05-30 02:45:05 +0200

    some fixes in install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/env bash
 
-# arg1: rootdir
-# arg2: install/remove
-# arg3: method
-# arg4 (opt): username
+
+# arg1: install/remove
+# arg2: method
+# arg3: (rootdir)
 # arg5 (opt): usrdir DON'T USE!!!!!!
+# arg4 (opt): username
 
 userid=921 #"$(id -u "$username")"
 username="lamysys"
@@ -12,24 +13,26 @@ filedir="/usr/share/lamysys"
 
 help()
 {
-  echo "help"
+  echo "$0 install/remove method (rootdir)"
   exit 1
 }
 
 
-if [ "$#" -le 3 ]; then
+if [ "$#" -lt 2 ]; then
   help
 fi
 
 projdir="$(dirname "$0")"
 
-rootdir="$(realpath($1))"
+
+
+install="$1"
+method="$2"
+rootdir="$(realpath($3))"
 if [ "$rootdir" = "/" ]; then
   rootdir=""
 fi
 
-install="$2"
-method="$3"
 if [ "$4" != "" ]; then
   username="$4"
 fi
@@ -55,7 +58,7 @@ udevinstall()
   if [ "$install" = "install" ]; then
     install -D -m755 "$projdir/launchmethods/lamysysudevhelper.sh" "$rootdir$filedir/lamysysudevhelper.sh"
 	install -D -m755 "$projdir/launchmethods/11-lamysys-start.rules"  "$rootdir/etc/udev/rules.d/11-lamysys-start.rules"
-    sed -i -e "s|uid=921|uid=$userid|" -e "s|/usr/share/lamysys/|$filedir|"
+    sed -i -e "s|uid=921|uid=$userid|" -e "s|/usr/share/lamysys/|$filedir|" "$rootdir/etc/udev/rules.d/11-lamysys-start.rules"
   elif [ "$install" = "uninstall" ]; then
     rm -r "$rootdir$filedir"
     rm "$rootdir/etc/udev/rules.d/11-lamysys-start.rules"
@@ -67,9 +70,12 @@ udevinstall()
 autostart()
 {
   if [ "$install" = "install" ]; then
-    
+    install -D -m755 "$projdir/launchmethods/lookformount.sh" "$rootdir$filedir/lookformount.sh"
+    install -D -m755 "$projdir/launchmethods/lamysysautostart.desktop"  "$rootdir/etc/xdg/autostart/lamysysautostart.desktop"
+    sed -i -e "s|/usr/share/lamysys/|$filedir|" "$rootdir/etc/xdg/autostart/lamysysautostart.desktop"
   elif [ "$install" = "uninstall" ]; then
-  
+    rm -r "$rootdir$filedir"
+    rm "$rootdir/etc/xdg/autostart/lamysysautostart.desktop"
   fi
 }
 
